89bd1655af708bbc950011a6ea993093204ab4e5
Guy Hassan <33221427+GuyHassan@users.noreply.github.com>
2019-04-22 18:09:19 +0300

    Update sudoku_validator.py

--- algorithms/matrix/sudoku_validator.py
+++ algorithms/matrix/sudoku_validator.py
@@ -9,34 +9,36 @@ The board is always 9 cells by 9 cells, and every cell only contains integers fr
 from collections import defaultdict
 def valid_solution_hashtable(board):
     for i in range(len(board)):
-        dict_row = defaultdict(int)
-        dict_col = defaultdict(int)
-        for j in range(len(board[0])):
-            value_row = board[i][j]
-            value_col = board[j][i]
-            if not value_row or value_col == 0:
-                return False
-            if value_row in dict_row:
-                return False
-            else:
-                dict_row[value_row] += 1
-
-            if value_col in dict_col:
-                return False
-            else:
-                dict_col[value_col] += 1
-
+        helpFunc1(board,i)
     for i in range(3):
         for j in range(3):
-            grid_add = 0
-            for k in range(3):
-                for l in range(3):
-                    grid_add += board[i*3+k][j*3+l]
-            if grid_add != 45:
+            if(not helpFunc2(board,i,j)):
                 return False
     return True
+def helpFunc1(board,i):
+    dict_row = defaultdict(int)
+    dict_col = defaultdict(int)
+    for j in range(len(board[0])):
+        value_row = board[i][j]
+        value_col = board[j][i]
+        if not value_row or value_col == 0:
+            return False
+        if value_row in dict_row:
+            return False
+        else:
+            dict_row[value_row] += 1
 
-
+        if value_col in dict_col:
+            return False
+        else:
+            dict_col[value_col] += 1
+def helpFunc2(board,i,j):
+    grid_add = 0
+    for k in range(3):
+        for l in range(3):
+            grid_add += board[i * 3 + k][j * 3 + l]
+    if grid_add != 45:
+        return False
 # Without hash-table/dict
 def valid_solution(board):
     correct = [1, 2, 3, 4, 5, 6, 7, 8, 9]
